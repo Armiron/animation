070ad3b1d21816f14ed8261d9e1126fd3ff091aa
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2023-01-31 23:25:03 +0100

    Mini animation

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -5,7 +5,25 @@ class Home extends StatefulWidget {
   HomeSate createState() => HomeSate();
 }
 
-class HomeSate extends State<Home> {
+class HomeSate extends State<Home> with TickerProviderStateMixin {
+  late Animation<double> catAnimation;
+  late AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0)
+        .animate(CurvedAnimation(parent: catController, curve: Curves.easeIn));
+
+    catController.forward();
+  }
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
@@ -16,6 +34,15 @@ class HomeSate extends State<Home> {
   }
 
   Widget buildAnimation() {
-    return Cat();
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(), //it gets given to the builder
+    );
   }
 }
